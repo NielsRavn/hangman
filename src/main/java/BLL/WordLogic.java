81026fcbe86a76174f61402074ff2da8954ce73c
niels <nkr@techraven.dk>
2015-03-23 08:43:00 +0100

    commit

---
+++ src/main/java/BLL/WordLogic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL;
+
+/**
+ *
+ * @author Niels
+ */
+public class WordLogic {
+    
+    
+    public boolean checkChar(char c){
+        return true == false;
+    }
+    
+    
+    public String getShowableWord(){
+        return null;
+    }
+    
+}
---
+++ src/test/java/BLL/WordLogicTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Niels
+ */
+public class WordLogicTest {
+    
+    public WordLogicTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of checkChar method, of class WordLogic.
+     */
+    @Test
+    public void testCheckChar() {
+        System.out.println("checkChar");
+        char c = ' ';
+        WordLogic instance = new WordLogic();
+        boolean expResult = false;
+        boolean result = instance.checkChar(c);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getShowableWord method, of class WordLogic.
+     */
+    @Test
+    public void testGetShowableWord() {
+        System.out.println("getShowableWord");
+        WordLogic instance = new WordLogic();
+        String expResult = "";
+        String result = instance.getShowableWord();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
